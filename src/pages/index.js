895bfa89f33a8abba7a1e55a999945d05a8b54a9
ravindasenarath <ravindasenarath@gmail.com>
2023-09-13 22:34:08 +1000

    changed message

--- src/pages/index.js
+++ src/pages/index.js
@@ -127,7 +127,7 @@ const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Hello world
+        Congratulations
         <br />
         <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
       </h1>
